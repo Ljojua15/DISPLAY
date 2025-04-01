edc170bdc069f0c0522b3465b66cf644c88cdd00
ljojua <ljojua@crocobet.com>
2025-04-01 11:06:38 +0400

    test

---
+++ src/app/lib/configs/header.config.ts
@@ -0,0 +1,19 @@
+import {IHeader} from '../interface/header.interface';
+
+export const HEADER_CONFIG:IHeader[] = [
+  {
+    pageName:'HOME',
+    isActive: false,
+    url:'home',
+  },
+  {
+    pageName:'CSS FLEX',
+    isActive: false,
+    url:'khinkali-flex',
+  },
+  {
+    pageName:'CSS GRID',
+    isActive: false,
+    url:'mtsvadi-grid',
+  }
+]
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import {IHeader} from '../../lib/interface/header.interface';
+import {HEADER_CONFIG} from '../../lib/configs/header.config';
+import {CommonModule} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'display-header',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  public readonly headers:IHeader[] = HEADER_CONFIG;
 
+  public setActivePage(header:IHeader){
+    this.headers.forEach((x) => x.isActive = false);
+    header.isActive = true;
+  }
 }
