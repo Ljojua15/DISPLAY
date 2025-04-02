ec2aec95d4ff005632030c75e0f54ec8422e4cce
ljojua <ljojua@crocobet.com>
2025-04-02 10:20:11 +0400

    add-footer

---
+++ src/app/lib/configs/contact.config.ts
@@ -0,0 +1,20 @@
+import {IContact} from '../interface/contact.interface';
+
+export const CONTACT_CONFIG: IContact[] = [
+  {
+    link:'',
+    icons:'./assets/images/icons/linkedin.png',
+  },
+  {
+    link:'',
+    icons:'./assets/images/icons/gmail.png',
+  },
+  {
+    link:'',
+    icons:'./assets/images/icons/whatsapp.png',
+  },
+  {
+    link:'',
+    icons:'./assets/images/icons/facebook.png',
+  },
+]
--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {IContact} from '../../lib/interface/contact.interface';
+import {CONTACT_CONFIG} from '../../lib/configs/contact.config';
 
 @Component({
   selector: 'display-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  public readonly contacts: IContact[] = CONTACT_CONFIG
 }
