bfb36bd4fddb8e97d0b809d52e766f01648d1e8b
ljojua <ljojua@crocobet.com>
2025-04-10 12:07:19 +0400

    prod

---
+++ src/app/comp-grid/result-grid/result-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'display-result-grid',
+  imports: [],
+  templateUrl: './result-grid.component.html',
+  styleUrl: './result-grid.component.scss'
+})
+export class ResultGridComponent {
+
+}
